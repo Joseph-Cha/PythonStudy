16845fe2879b5b44eb99ee99c9cf717e88181431
Joseph.c <joseph.c@kakao.com>
2023-02-03 22:21:36 +0900

    8강 class 수강 중(메서드 오버라이딩)

--- #8.class/class.py
+++ #8.class/class.py
@@ -1,15 +1,20 @@
 # 일반 유닛
 class Unit:
-    def __init__(self, name, hp): # 생성자
+    def __init__(self, name, hp, speed): # 생성자
         # self.멤버 변수
         self.name = name
         self.hp = hp
+        self.speed = speed
+    def move(self, location):
+        print("[지상 유닉 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}"\
+            .format(self.name, location, self.speed))
 
 # 공격 유닛
 class AttackUnit(Unit): # 상속
-    def __init__(self, name, hp, damage): # 생성자
+    def __init__(self, name, hp, damage, speed): # 생성자
         # self.멤버 변수
-        Unit.__init__(self, name, hp)
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
 
     def attack(self, location):
@@ -37,8 +42,12 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, damage, 0) # 지상 speed 0
         Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
 
 # 발키리
 valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
@@ -66,4 +75,11 @@ firebat1.attack("5시")
 firebat1.damaged(25)
 firebat1.damaged(25)
 
-# 드랍쉽
+# 벌처
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# 배틀크루저
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+battlecruiser.move("9시")
